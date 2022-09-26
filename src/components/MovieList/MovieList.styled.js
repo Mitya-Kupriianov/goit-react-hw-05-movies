35467ffd33f6d1cc1c60f82c807b9914f13fc1db
Mitya-Kupriianov <MityaBlow@gmail.com>
2022-09-26 23:36:54 +0300

    fix

--- src/components/MovieList/MovieList.styled.js
+++ src/components/MovieList/MovieList.styled.js
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fit, 280px);
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
   gap: 20px;
-  padding-left: 40px;
+  padding: 0;
+  margin: 0px auto;
 `;
 
-export const CardWrapper = styled.div`
+export const CardWrapper = styled.li`
   border: 1px solid black;
   border-radius: 4px;
-
+  list-style: none;
   > a {
     text-decoration: none;
   }
@@ -24,6 +25,6 @@ export const MovieName = styled.h3`
   text-align: center;
 `;
 export const Img = styled.img`
-  width: 278px;
-  height: 160px;
+  width: 100%;
+  height: auto;
 `;
